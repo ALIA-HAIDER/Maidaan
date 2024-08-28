3f8b51244fde93e3b8f0c832c67c3e1ac18c26c9
ALIA-HAIDER <turabhaider786@outlook.com>
2024-08-28 09:23:20 +0530

    ChampionsSchedule done

---
+++ src/pages/TournamentsIndividualSchedule.tsx
@@ -0,0 +1,16 @@
+
+import { FooterLinks } from "@/components/LpFooter/FooterLinks"
+import { HeaderMegaMenu } from "@/components/MaidaanLogo/HeaderMegaMenu"
+import ChampionsSchedule from "@/components/TouIndividualSchedule/ChampionsSchedule"
+
+export default function TournamentsIndividualSchedule() {
+  return (
+   <>
+   <HeaderMegaMenu/>
+   <ChampionsSchedule/>
+
+   <FooterLinks/>
+
+   </>
+  )
+}

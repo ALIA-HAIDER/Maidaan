95dc7486f8a93b532403dcda7d54a60aa79543b6
Mayank Gupta <mayankg88813@gmail.com>
2024-08-28 23:25:45 +0530

    Small Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import { HeaderMegaMenu } from './components/MaidaanLogo/HeaderMegaMenu';
+import { FooterLinks } from './components/LpFooter/FooterLinks';
 
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
+      <HeaderMegaMenu/>
       <Router />
+      <FooterLinks/>
     </MantineProvider>
   );
 }

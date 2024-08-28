4b91fd8568493cbe4ce0535b83e6714c4f07edc6
ALIA-HAIDER <turabhaider786@outlook.com>
2024-08-28 07:33:29 +0530

    championsTrophy page done

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,32 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import { TournamentDetailPage } from './pages/TournamentDetailPage';
+import {TournamentpagePrize} from './pages/TouranmentpagePrize';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
+
 
 // const router = createBrowserRouter([
 //   {
 //     path: '/',
-//     element: <HomePage />,
+//     element: <TournamentDetailPage />,
 //   },
 // ]);
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <TournamentDetailPage />,
-  },
-]);
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <TournamentpagePrize/>  ,
+//   },
+// ]);
+
+
 export function Router() {
   return <RouterProvider router={router} />;
 }
